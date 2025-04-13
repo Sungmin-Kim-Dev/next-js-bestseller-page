6572f9ff87eaabaaa45abd6bd9fb7685f5d59007
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-14 06:05:08 +0900

    not found page function, about page design

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About This Website</h1>
-      <p>This is my second Next.js project.</p>
+    <div className="text-center mt-10">
+      <h1 className="text-4xl">About This Website</h1>
+      <p className="text-2xl">This is my second Next.js project.</p>
     </div>
   );
 };
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
 const NotFoundPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
   return (
-    <div className="h-dvh bg-amber-300/10">
+    <div className="h-dvh">
       <h1 className="text-center text-4xl pt-20">Not Found</h1>
       <h2 className="text-center">Redirecting to Main Page in 5 seconds...</h2>
     </div>
